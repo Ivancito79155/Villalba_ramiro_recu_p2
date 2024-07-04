86363df94843d0c91246c0f2d3f86728a25f4620
Iván Ramiro Villalba <ramiroivan79@hotmail.com>
2024-07-04 15:36:54 -0300

    se arreglaron errores de sintaxis

--- src/app.js
+++ src/app.js
@@ -4,6 +4,6 @@ const app = express();
 
 app.use(express.json());
 
-app.use(require("./routers/crud.controllers"))
+app.use(require("./routers/crud.routers"))
 
 app.listen(4321, console.log("SERVIDOR CORRIENDO EN PUERTO 4321"));
